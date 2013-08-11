3bf7a6ba90597bd3715c3aaa881ae98f1a509212
Martin Lau <m@hum.ph>
2013-08-11 21:32:57 +1000

    Added override for location details

--- src/main/java/org/un_idle/controller/HomeController.java
+++ src/main/java/org/un_idle/controller/HomeController.java
@@ -2,8 +2,10 @@ package org.un_idle.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.un_idle.domain.LocationFact;
 import org.un_idle.geo.Location;
 import org.un_idle.service.LocationFactService;
@@ -25,11 +27,34 @@ public class HomeController {
     }
 
     @ModelAttribute("locationFact")
-    public LocationFact locationFact(final Location location) {
-        return locationFactService.findBestFact(location.getCity(),
-                                                location.getSubdivision(),
-                                                location.getCountry(),
-                                                location.getContinent());
+    public LocationFact locationFact(final Location location,
+                                     @RequestParam(value = "city",
+                                                   defaultValue = "")
+                                     final String city,
+                                     @RequestParam(value = "subdivision",
+                                                   defaultValue = "")
+                                     final String subdivision,
+                                     @RequestParam(value = "country",
+                                                   defaultValue = "")
+                                     final String country,
+                                     @RequestParam(value = "continent",
+                                                   defaultValue = "")
+                                     final String continent) {
+
+        return StringUtils.hasText(city)
+               || StringUtils.hasText(subdivision)
+               || StringUtils.hasText(country)
+               || StringUtils.hasText(continent)
+
+               ? locationFactService.findBestFact(city,
+                                                  subdivision,
+                                                  country,
+                                                  continent)
+
+               : locationFactService.findBestFact(location.getCity(),
+                                                  location.getSubdivision(),
+                                                  location.getCountry(),
+                                                  location.getContinent());
     }
 
 }
