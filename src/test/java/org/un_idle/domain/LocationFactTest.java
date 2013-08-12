4d1473d2c9b245149ee6ee92f730be4a5c24c618
Martin Lau <m@hum.ph>
2013-08-13 08:13:17 +1000

    Added LocationFactTest

---
+++ src/test/java/org/un_idle/domain/LocationFactTest.java
@@ -0,0 +1,60 @@
+package org.un_idle.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class LocationFactTest {
+
+    private LocationFact subject;
+
+    @Before
+    public void setUp() throws Exception {
+        subject = new LocationFact();
+    }
+
+    @Test
+    public void testGetLocationWithCity() throws Exception {
+        subject.setCity("city");
+
+        final String result = subject.getLocation();
+
+        assertThat(result).isEqualTo("city");
+    }
+
+    @Test
+    public void testGetLocationWithSubdivision() throws Exception {
+        subject.setSubdivision("subdivision");
+
+        final String result = subject.getLocation();
+
+        assertThat(result).isEqualTo("subdivision");
+    }
+
+    @Test
+    public void testGetLocationWithCountry() throws Exception {
+        subject.setCountry("country");
+
+        final String result = subject.getLocation();
+
+        assertThat(result).isEqualTo("country");
+    }
+
+    @Test
+    public void testGetLocationWithContent() throws Exception {
+        subject.setContinent("continent");
+
+        final String result = subject.getLocation();
+
+        assertThat(result).isEqualTo("continent");
+    }
+
+    @Test
+    public void testGetLocationWithNothing() throws Exception {
+        final String result = subject.getLocation();
+
+        assertThat(result).isNull();
+    }
+
+}
