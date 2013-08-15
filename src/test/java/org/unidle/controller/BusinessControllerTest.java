873b1ac7e89b3d643008c53aea87f4148fb5cb37
Martin Lau <m@hum.ph>
2013-08-15 22:08:31 +1000

    Added business controller test

---
+++ src/test/java/org/unidle/controller/BusinessControllerTest.java
@@ -0,0 +1,44 @@
+package org.unidle.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.unidle.config.MvcConfiguration;
+import org.unidle.config.RootContextConfiguration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@ContextHierarchy({@ContextConfiguration(classes = RootContextConfiguration.class),
+                   @ContextConfiguration(classes = MvcConfiguration.class)})
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class BusinessControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testBusiness() throws Exception {
+        mockMvc.perform(get("/business"))
+               .andExpect(status().isOk())
+               .andExpect(view().name(".static.business"));
+    }
+
+}
