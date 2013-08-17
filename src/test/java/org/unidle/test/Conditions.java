171c674f1376f18e10b9469cc7b0552c41918d4f
Martin Lau <m@hum.ph>
2013-08-17 22:40:22 +1000

    Updated conditions to use reflection

--- src/test/java/org/unidle/test/Conditions.java
+++ src/test/java/org/unidle/test/Conditions.java
@@ -1,45 +1,15 @@
 package org.unidle.test;
 
 import org.fest.assertions.Condition;
-import org.unidle.geo.HasCity;
-import org.unidle.geo.HasContinent;
-import org.unidle.geo.HasCountry;
-import org.unidle.geo.HasSubdivision;
-import org.unidle.domain.LocationFact;
 import org.unidle.domain.TimeUnit;
+import org.unidle.domain.User;
 
 import java.util.Map;
 
 import static java.lang.String.format;
+import static org.springframework.test.util.ReflectionTestUtils.getField;
 
 public class Conditions {
-
-    public static Condition<Object> hasCity(final String city) {
-        return new Condition<Object>(format("has city: %s", city)) {
-            @Override
-            public boolean matches(final Object value) {
-                return city.equals(((HasCity) value).getCity());
-            }
-        };
-    }
-
-    public static Condition<Object> hasContinent(final String continent) {
-        return new Condition<Object>(format("has continent: %s", continent)) {
-            @Override
-            public boolean matches(final Object value) {
-                return continent.equals(((HasContinent) value).getContinent());
-            }
-        };
-    }
-
-    public static Condition<Object> hasCountry(final String country) {
-        return new Condition<Object>(format("has country: %s", country)) {
-            @Override
-            public boolean matches(final Object value) {
-                return country.equals(((HasCountry) value).getCountry());
-            }
-        };
-    }
 
     public static Condition<Map<?, ?>> containsKey(final Object key) {
         return new Condition<Map<?, ?>>(format("contains key: %s", key)) {
@@ -50,11 +20,137 @@ public class Conditions {
         };
     }
 
+    public static Condition<Object> hasAccessToken(final String accessToken) {
+        return new Condition<Object>(format("has access token: %s", accessToken)) {
+            @Override
+            public boolean matches(final Object value) {
+                return accessToken.equals(getField(value, "accessToken"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasCity(final String city) {
+        return new Condition<Object>(format("has city: %s", city)) {
+            @Override
+            public boolean matches(final Object value) {
+                return city.equals(getField(value, "city"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasContinent(final String continent) {
+        return new Condition<Object>(format("has continent: %s", continent)) {
+            @Override
+            public boolean matches(final Object value) {
+                return continent.equals(getField(value, "continent"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasCountry(final String country) {
+        return new Condition<Object>(format("has country: %s", country)) {
+            @Override
+            public boolean matches(final Object value) {
+                return country.equals(getField(value, "country"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasDisplayName(final String displayName) {
+        return new Condition<Object>(format("has display name: %s", displayName)) {
+            @Override
+            public boolean matches(final Object value) {
+                return displayName.equals(getField(value, "displayName"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasExpireTime(final Long expireTime) {
+        return new Condition<Object>(format("has expire time: %s", expireTime)) {
+            @Override
+            public boolean matches(final Object value) {
+                return expireTime.equals(getField(value, "expireTime"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasImageUrl(final String imageUrl) {
+        return new Condition<Object>(format("has image url: %s", imageUrl)) {
+            @Override
+            public boolean matches(final Object value) {
+                return imageUrl.equals(getField(value, "imageUrl"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasProfileUrl(final String profileUrl) {
+        return new Condition<Object>(format("has profile url: %s", profileUrl)) {
+            @Override
+            public boolean matches(final Object value) {
+                return profileUrl.equals(getField(value, "profileUrl"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasProviderId(final String providerId) {
+        return new Condition<Object>(format("has provider id: %s", providerId)) {
+            @Override
+            public boolean matches(final Object value) {
+                return providerId.equals(getField(value, "providerId"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasProviderUserId(final String providerUserId) {
+        return new Condition<Object>(format("has provider user id: %s", providerUserId)) {
+            @Override
+            public boolean matches(final Object value) {
+                return providerUserId.equals(getField(value, "providerUserId"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasConnectionKeyProviderUserId(final String providerUserId) {
+        return new Condition<Object>(format("has connection key provider user id: %s", providerUserId)) {
+            @Override
+            public boolean matches(final Object value) {
+                return providerUserId.equals(getField(getField(value, "key"), "providerUserId"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasConnectionKeyProviderId(final String providerId) {
+        return new Condition<Object>(format("has connection key provider id: %s", providerId)) {
+            @Override
+            public boolean matches(final Object value) {
+                return providerId.equals(getField(getField(value, "key"), "providerId"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasRefreshToken(final String refreshToken) {
+        return new Condition<Object>(format("has refresh token: %s", refreshToken)) {
+            @Override
+            public boolean matches(final Object value) {
+                return refreshToken.equals(getField(value, "refreshToken"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasSecret(final String secret) {
+        return new Condition<Object>(format("has secret: %s", secret)) {
+            @Override
+            public boolean matches(final Object value) {
+                return secret.equals(getField(value, "secret"));
+            }
+        };
+    }
+
     public static Condition<Object> hasSource(final String source) {
         return new Condition<Object>(format("has source: %s", source)) {
             @Override
             public boolean matches(final Object value) {
-                return source.equals(((LocationFact) value).getSource());
+                return source.equals(getField(value, "source"));
             }
         };
     }
@@ -63,7 +159,7 @@ public class Conditions {
         return new Condition<Object>(format("has subdivision: %s", subdivision)) {
             @Override
             public boolean matches(final Object value) {
-                return subdivision.equals(((HasSubdivision) value).getSubdivision());
+                return subdivision.equals(getField(value, "subdivision"));
             }
         };
     }
@@ -72,7 +168,7 @@ public class Conditions {
         return new Condition<Object>(format("has summary duration: %s", summaryDuration)) {
             @Override
             public boolean matches(final Object value) {
-                return summaryDuration.equals(((LocationFact) value).getSummaryDuration());
+                return summaryDuration.equals(getField(value, "summaryDuration"));
             }
         };
     }
@@ -81,7 +177,7 @@ public class Conditions {
         return new Condition<Object>(format("has summary duration time unit: %s", summaryDurationTimeUnit)) {
             @Override
             public boolean matches(final Object value) {
-                return summaryDurationTimeUnit.equals(((LocationFact) value).getSummaryDurationTimeUnit());
+                return summaryDurationTimeUnit.equals(getField(value, "summaryDurationTimeUnit"));
             }
         };
     }
@@ -90,7 +186,7 @@ public class Conditions {
         return new Condition<Object>(format("has task code: %s", taskCode)) {
             @Override
             public boolean matches(final Object value) {
-                return taskCode.equals(((LocationFact) value).getTaskCode());
+                return taskCode.equals(getField(value, "taskCode"));
             }
         };
     }
@@ -99,7 +195,7 @@ public class Conditions {
         return new Condition<Object>(format("has task duration: %s", taskDuration)) {
             @Override
             public boolean matches(final Object value) {
-                return taskDuration.equals(((LocationFact) value).getTaskDuration());
+                return taskDuration.equals(getField(value, "taskDuration"));
             }
         };
     }
@@ -108,7 +204,7 @@ public class Conditions {
         return new Condition<Object>(format("has task duration time unit: %s", taskDurationTimeUnit)) {
             @Override
             public boolean matches(final Object value) {
-                return taskDurationTimeUnit.equals(((LocationFact) value).getTaskDurationTimeUnit());
+                return taskDurationTimeUnit.equals(getField(value, "taskDurationTimeUnit"));
             }
         };
     }
@@ -117,7 +213,7 @@ public class Conditions {
         return new Condition<Object>(format("has task people: %s", taskPeople)) {
             @Override
             public boolean matches(final Object value) {
-                return taskPeople.equals(((LocationFact) value).getTaskPeople());
+                return taskPeople.equals(getField(value, "taskPeople"));
             }
         };
     }
@@ -126,9 +222,28 @@ public class Conditions {
         return new Condition<Object>(format("has task time unit: %s", taskTimeUnit)) {
             @Override
             public boolean matches(final Object value) {
-                return taskTimeUnit.equals(((LocationFact) value).getTaskTimeUnit());
+                return taskTimeUnit.equals(getField(value, "taskTimeUnit"));
             }
         };
     }
+
+    public static Condition<Object> hasUser(final User user) {
+        return new Condition<Object>(format("has user: %s", user)) {
+            @Override
+            public boolean matches(final Object value) {
+                return user.equals(getField(value, "user"));
+            }
+        };
+    }
+
+    public static Condition<Object> hasRank(final Integer rank) {
+        return new Condition<Object>(format("has rank: %d", rank)) {
+            @Override
+            public boolean matches(final Object value) {
+                return rank.equals(getField(value, "rank"));
+            }
+        };
+    }
+
 
 }
