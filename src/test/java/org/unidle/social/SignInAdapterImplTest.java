ab6f54ebf01eaec9cda70c9617010868c07e5123
Martin Lau <m@hum.ph>
2013-08-17 20:06:31 +1000

    Added SignInAdapterImplTest

---
+++ src/test/java/org/unidle/social/SignInAdapterImplTest.java
@@ -0,0 +1,47 @@
+package org.unidle.social;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.social.connect.web.SignInAdapter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.unidle.config.RootContextConfiguration;
+import org.unidle.config.SocialConfiguration;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+@ContextHierarchy({@ContextConfiguration(classes = RootContextConfiguration.class),
+                   @ContextConfiguration(classes = SocialConfiguration.class)})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SignInAdapterImplTest {
+
+    @Autowired
+    private SignInAdapter signInAdapter;
+
+    @Before
+    public void setUp() throws Exception {
+        SecurityContextHolder.clearContext();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        SecurityContextHolder.clearContext();
+    }
+
+    @Test
+    public void testSignIn() throws Exception {
+        signInAdapter.signIn("userId", null, null);
+
+        final Object principal = SecurityContextHolder.getContext()
+                                                      .getAuthentication()
+                                                      .getPrincipal();
+
+        assertThat(principal).isEqualTo("userId");
+    }
+
+}
