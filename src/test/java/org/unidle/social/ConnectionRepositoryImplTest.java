df5603035c34d9d392e7ba329e9c2fbf2bc52a98
Martin Lau <m@hum.ph>
2013-08-18 20:11:15 +1000

    added the rest of ConnectionRepositoryImpl's tests

--- src/test/java/org/unidle/social/ConnectionRepositoryImplTest.java
+++ src/test/java/org/unidle/social/ConnectionRepositoryImplTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionData;
+import org.springframework.social.connect.ConnectionKey;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.NoSuchConnectionException;
 import org.springframework.social.connect.NotConnectedException;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.twitter.api.Twitter;
@@ -13,6 +16,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.unidle.config.RootContextConfiguration;
 import org.unidle.config.SocialConfiguration;
@@ -20,11 +24,15 @@ import org.unidle.domain.User;
 import org.unidle.domain.UserConnection;
 import org.unidle.repository.UserConnectionRepository;
 import org.unidle.repository.UserRepository;
+import org.unidle.social.test.ConnectionStub;
 
 import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 import static org.unidle.test.Conditions.containsKey;
+import static org.unidle.test.Conditions.hasConnectionKeyProviderId;
+import static org.unidle.test.Conditions.hasConnectionKeyProviderUserId;
 import static org.unidle.test.Conditions.hasProviderId;
 import static org.unidle.test.Conditions.hasProviderUserId;
 
@@ -74,11 +82,23 @@ public class ConnectionRepositoryImplTest {
         subject = connectionRepositoryFactory.getConnectionRepository(user.getId().toString());
     }
 
-//    @Test
-//    public void testAddConnection() throws Exception {
-//
-//        subject.addConnection(connection);
-//    }
+    @Test
+    public void testAddConnection() throws Exception {
+        final ConnectionData connectionData = new ConnectionData("twitter",
+                                                                 "provider user id 3",
+                                                                 "display name",
+                                                                 "profile url",
+                                                                 "image url",
+                                                                 "access token",
+                                                                 "secret",
+                                                                 "refresh token",
+                                                                 1234L);
+        final Connection<?> connection = new ConnectionStub<>(connectionData);
+
+        subject.addConnection(connection);
+
+        assertThat(userConnectionRepository.findAll(user, "twitter")).hasSize(3);
+    }
 
     @Test
     public void testFindAllConnections() throws Exception {
@@ -89,11 +109,27 @@ public class ConnectionRepositoryImplTest {
                 .satisfies(containsKey("twitter"));
     }
 
-//    @Test
-//    public void testFindConnectionsToUsers() throws Exception {
-//
-//        subject.findConnectionsToUsers(providerUserIds);
-//    }
+    @Test
+    public void testFindConnectionsToUsers() throws Exception {
+        final MultiValueMap<String, String> providerUserIds = new LinkedMultiValueMap<>();
+        providerUserIds.add("twitter", "provider user id 1");
+        providerUserIds.add("twitter", "provider user id 2");
+        providerUserIds.add("twitter", "provider user id 3");
+        providerUserIds.add("facebook", "provider user id 4");
+
+        final MultiValueMap<String, Connection<?>> result = subject.findConnectionsToUsers(providerUserIds);
+
+        assertThat(result)
+                .hasSize(2)
+                .satisfies(containsKey("twitter"))
+                .satisfies(containsKey("facebook"));
+        assertThat(result.get("twitter")).hasSize(3);
+        assertThat(result.get("twitter").get(0)).isNotNull();
+        assertThat(result.get("twitter").get(1)).isNotNull();
+        assertThat(result.get("twitter").get(2)).isNull();
+        assertThat(result.get("facebook")).hasSize(1);
+        assertThat(result.get("facebook").get(0)).isNull();
+    }
 
     @Test
     public void testFindConnectionsWithClass() throws Exception {
@@ -134,11 +170,25 @@ public class ConnectionRepositoryImplTest {
                 .satisfies(hasProviderUserId("provider user id 1"));
     }
 
-//    @Test
-//    public void testGetConnectionWithConnectionKey() throws Exception {
-//
-//        subject.getConnection(connectionKey);
-//    }
+    @Test
+    public void testGetConnectionWithConnectionKey() throws Exception {
+        final ConnectionKey connectionKey = new ConnectionKey("twitter",
+                                                              "provider user id 1");
+
+        final Connection<?> result = subject.getConnection(connectionKey);
+
+        assertThat(result)
+                .satisfies(hasConnectionKeyProviderId("twitter"))
+                .satisfies(hasConnectionKeyProviderUserId("provider user id 1"));
+    }
+
+    @Test(expected = NoSuchConnectionException.class)
+    public void testGetConnectionWithConnectionKeyWithUnknownConnection() throws Exception {
+
+        final ConnectionKey connectionKey = new ConnectionKey("twitter",
+                                                              "unknown provider user id");
+        subject.getConnection(connectionKey);
+    }
 
     @Test
     public void testGetPrimaryConnection() throws Exception {
@@ -154,11 +204,14 @@ public class ConnectionRepositoryImplTest {
         subject.getPrimaryConnection(Facebook.class);
     }
 
-//    @Test
-//    public void testRemoveConnection() throws Exception {
-//
-//        subject.removeConnection(connection);
-//    }
+    @Test
+    public void testRemoveConnection() throws Exception {
+        final Connection<Twitter> connection = subject.findPrimaryConnection(Twitter.class);
+
+        subject.removeConnection(connection.getKey());
+
+        assertThat(userConnectionRepository.findAll(user)).hasSize(1);
+    }
 
     @Test
     public void testRemoveConnections() throws Exception {
@@ -167,10 +220,15 @@ public class ConnectionRepositoryImplTest {
         assertThat(userConnectionRepository.count()).isZero();
     }
 
-//    @Test
-//    public void testUpdateConnection() throws Exception {
-//
-//        subject.updateConnection(connection);
-//    }
+    @Test
+    public void testUpdateConnection() throws Exception {
+        final Connection<Twitter> connection = subject.getPrimaryConnection(Twitter.class);
+        setField(connection, "accessToken", "new access token");
+        setField(connection, "secret", "new secret");
+
+        subject.updateConnection(connection);
+
+        assertThat(userConnectionRepository.findPrimaryConnection(user, "twitter").getRevision()).isEqualTo(1);
+    }
 
 }
