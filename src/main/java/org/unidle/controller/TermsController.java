ace10b4e704c82069274947afe1e6c5bbab449c7
Martin Lau <m@hum.ph>
2013-08-19 21:56:01 +1000

    Added terms of use

---
+++ src/main/java/org/unidle/controller/TermsController.java
@@ -0,0 +1,14 @@
+package org.unidle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TermsController {
+
+    @RequestMapping("/terms")
+    public String terms() {
+        return ".terms.index";
+    }
+
+}
---
+++ src/test/java/org/unidle/controller/TermsControllerTest.java
@@ -0,0 +1,44 @@
+package org.unidle.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.unidle.config.MvcConfiguration;
+import org.unidle.config.RootConfiguration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@ContextHierarchy({@ContextConfiguration(classes = RootConfiguration.class),
+                   @ContextConfiguration(classes = MvcConfiguration.class)})
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class TermsControllerTest {
+
+    private MockMvc subject;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() throws Exception {
+        subject = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testTerms() throws Exception {
+        subject.perform(get("/terms"))
+               .andExpect(status().isOk())
+               .andExpect(view().name(".terms.index"));
+    }
+
+}
