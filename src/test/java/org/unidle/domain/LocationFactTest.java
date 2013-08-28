3bdf59712bb2a0cd4396a0b9fd5539a477abb2cb
Martin Lau <m@hum.ph>
2013-08-28 13:23:39 +1000

    Added more location fact tests

--- src/test/java/org/unidle/domain/LocationFactTest.java
+++ src/test/java/org/unidle/domain/LocationFactTest.java
@@ -22,8 +22,11 @@ package org.unidle.domain;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.unidle.domain.LocationFact.TimeUnit;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.springframework.test.util.ReflectionTestUtils.getField;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 public class LocationFactTest {
 
@@ -32,6 +35,42 @@ public class LocationFactTest {
     @Before
     public void setUp() throws Exception {
         subject = new LocationFact();
+    }
+
+    @Test
+    public void testGetCity() throws Exception {
+        setField(subject, "city", "city");
+
+        final String result = subject.getCity();
+
+        assertThat(result).isEqualTo("city");
+    }
+
+    @Test
+    public void testGetContinent() throws Exception {
+        setField(subject, "continent", "continent");
+
+        final String result = subject.getContinent();
+
+        assertThat(result).isEqualTo("continent");
+    }
+
+    @Test
+    public void testGetCountry() throws Exception {
+        setField(subject, "country", "country");
+
+        final String result = subject.getCountry();
+
+        assertThat(result).isEqualTo("country");
+    }
+
+    @Test
+    public void testGetLocationPrefix() throws Exception {
+        setField(subject, "locationPrefix", "location prefix");
+
+        final String result = subject.getLocationPrefix();
+
+        assertThat(result).isEqualTo("location prefix");
     }
 
     @Test
@@ -44,12 +83,12 @@ public class LocationFactTest {
     }
 
     @Test
-    public void testGetLocationWithSubdivision() throws Exception {
-        subject.setSubdivision("subdivision");
+    public void testGetLocationWithContent() throws Exception {
+        subject.setContinent("continent");
 
         final String result = subject.getLocation();
 
-        assertThat(result).isEqualTo("subdivision");
+        assertThat(result).isEqualTo("continent");
     }
 
     @Test
@@ -62,19 +101,204 @@ public class LocationFactTest {
     }
 
     @Test
-    public void testGetLocationWithContent() throws Exception {
-        subject.setContinent("continent");
-
-        final String result = subject.getLocation();
-
-        assertThat(result).isEqualTo("continent");
-    }
-
-    @Test
     public void testGetLocationWithNothing() throws Exception {
         final String result = subject.getLocation();
 
         assertThat(result).isNull();
+    }
+
+    @Test
+    public void testGetLocationWithSubdivision() throws Exception {
+        subject.setSubdivision("subdivision");
+
+        final String result = subject.getLocation();
+
+        assertThat(result).isEqualTo("subdivision");
+    }
+
+    @Test
+    public void testGetSource() throws Exception {
+        setField(subject, "source", "source");
+
+        final String result = subject.getSource();
+
+        assertThat(result).isEqualTo("source");
+    }
+
+    @Test
+    public void testGetSubdivision() throws Exception {
+        setField(subject, "subdivision", "subdivision");
+
+        final String result = subject.getSubdivision();
+
+        assertThat(result).isEqualTo("subdivision");
+    }
+
+    @Test
+    public void testGetSummaryDuration() throws Exception {
+        setField(subject, "summaryDuration", 1234);
+
+        final Integer result = subject.getSummaryDuration();
+
+        assertThat(result).isEqualTo(1234);
+    }
+
+    @Test
+    public void testGetSummaryDurationTimeUnit() throws Exception {
+        setField(subject, "summaryDurationTimeUnit", TimeUnit.DAY);
+
+        final TimeUnit result = subject.getSummaryDurationTimeUnit();
+
+        assertThat(result).isEqualTo(TimeUnit.DAY);
+    }
+
+    @Test
+    public void testGetTaskCode() throws Exception {
+        setField(subject, "taskCode", "task code");
+
+        final String result = subject.getTaskCode();
+
+        assertThat(result).isEqualTo("task code");
+    }
+
+    @Test
+    public void testGetTaskDuration() throws Exception {
+        setField(subject, "taskDuration", 1234);
+
+        final Integer result = subject.getTaskDuration();
+
+        assertThat(result).isEqualTo(1234);
+    }
+
+    @Test
+    public void testGetTaskDurationTimeUnit() throws Exception {
+        setField(subject, "taskDurationTimeUnit", TimeUnit.DAY);
+
+        final TimeUnit result = subject.getTaskDurationTimeUnit();
+
+        assertThat(result).isEqualTo(TimeUnit.DAY);
+    }
+
+    @Test
+    public void testGetTaskPeople() throws Exception {
+        setField(subject, "taskPeople", 1234);
+
+        final Integer result = subject.getTaskPeople();
+
+        assertThat(result).isEqualTo(1234);
+    }
+
+    @Test
+    public void testGetTaskTimeUnit() throws Exception {
+        setField(subject, "taskTimeUnit", TimeUnit.DAY);
+
+        final TimeUnit result = subject.getTaskTimeUnit();
+
+        assertThat(result).isEqualTo(TimeUnit.DAY);
+    }
+
+    @Test
+    public void testSetCity() throws Exception {
+        subject.setCity("city");
+
+        final Object result = getField(subject, "city");
+        assertThat(result).isEqualTo("city");
+    }
+
+    @Test
+    public void testSetContinent() throws Exception {
+        subject.setContinent("continent");
+
+        final Object result = getField(subject, "continent");
+        assertThat(result).isEqualTo("continent");
+    }
+
+    @Test
+    public void testSetCountry() throws Exception {
+        subject.setCountry("country");
+
+        final Object result = getField(subject, "country");
+        assertThat(result).isEqualTo("country");
+    }
+
+    @Test
+    public void testSetLocationPrefix() throws Exception {
+        subject.setLocationPrefix("location prefix");
+
+        final Object result = getField(subject, "locationPrefix");
+        assertThat(result).isEqualTo("location prefix");
+    }
+
+    @Test
+    public void testSetSource() throws Exception {
+        subject.setSource("source");
+
+        final Object result = getField(subject, "source");
+        assertThat(result).isEqualTo("source");
+    }
+
+    @Test
+    public void testSetSubdivision() throws Exception {
+        subject.setSubdivision("subdivision");
+
+        final Object result = getField(subject, "subdivision");
+        assertThat(result).isEqualTo("subdivision");
+    }
+
+    @Test
+    public void testSetSummaryDuration() throws Exception {
+        subject.setSummaryDuration(1234);
+
+        final Object result = getField(subject, "summaryDuration");
+        assertThat(result).isEqualTo(1234);
+    }
+
+    @Test
+    public void testSetSummaryDurationTimeUnit() throws Exception {
+        subject.setSummaryDurationTimeUnit(TimeUnit.DAY);
+
+        final Object result = getField(subject, "summaryDurationTimeUnit");
+        assertThat(result).isEqualTo(TimeUnit.DAY);
+    }
+
+    @Test
+    public void testSetTaskCode() throws Exception {
+        subject.setTaskCode("task code");
+
+        final Object result = getField(subject, "taskCode");
+        assertThat(result).isEqualTo("task code");
+    }
+
+    @Test
+    public void testSetTaskDuration() throws Exception {
+        subject.setTaskDuration(1234);
+
+        final Object result = getField(subject, "taskDuration");
+        assertThat(result).isEqualTo(1234);
+    }
+
+    @Test
+    public void testSetTaskDurationTimeUnit() throws Exception {
+        subject.setTaskDurationTimeUnit(TimeUnit.DAY);
+
+        final Object result = getField(subject, "taskDurationTimeUnit");
+        assertThat(result).isEqualTo(TimeUnit.DAY);
+    }
+
+    @Test
+    public void testSetTaskPeople() throws Exception {
+        subject.setTaskPeople(1234);
+
+        final Object result = getField(subject, "taskPeople");
+        assertThat(result).isEqualTo(1234);
+    }
+
+    @Test
+    public void testSetTaskTimeUnit() throws Exception {
+        subject.setTaskTimeUnit(TimeUnit.DAY);
+
+        final Object result = getField(subject, "taskTimeUnit");
+        assertThat(result).isEqualTo(TimeUnit.DAY);
     }
 
 }
