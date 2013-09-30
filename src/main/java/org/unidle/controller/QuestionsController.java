a0d35ae8f065491b913b503def2c2bf639c95220
Martin Lau <m@hum.ph>
2013-10-01 08:50:27 +1000

    Added questions controller and test

---
+++ src/main/java/org/unidle/controller/QuestionsController.java
@@ -0,0 +1,56 @@
+/*
+ * #%L
+ * unidle
+ * %%
+ * Copyright (C) 2013 Martin Lau
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.unidle.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.unidle.domain.Question;
+import org.unidle.repository.QuestionRepository;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
+@Controller
+public class QuestionsController {
+
+    // TODO Replace with QuestionService
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionsController(final QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    @RequestMapping("/questions")
+    public String questions() {
+        return ".questions";
+    }
+
+    @ModelAttribute("questions")
+    public Page<Question> questions(@PageableDefault(size = 5, sort = "createdDate", direction = DESC) final Pageable pageable) {
+        return questionRepository.findAll(pageable);
+    }
+
+}
